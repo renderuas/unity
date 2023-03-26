fba5d0ab79238d4d5fa2890148687c3dca13b7d8
renderuas <renderuas@gmail.com>
2023-03-26 13:17:12 +0200

    Add files via upload

---
+++ test.cs
@@ -0,0 +1,28 @@
+//test script for unity
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("left"))
+        {
+            gameobject.transform.Translate(-50f * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey("right"))
+        {
+            gameobject.transform.Translate(50f * Time.deltaTime, 0, 0);
+        }
+        
+    }
+}
